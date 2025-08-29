5a24489a7b0bf0c406d291dd51e82a085d338d44
Fabio Sarracino <info@enemyx.net>
2025-08-30 00:48:41 +0200

    Added the Free Memory Node

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -7,5 +7,11 @@ This module contains all the ComfyUI nodes for VibeVoice integration.
 from .load_text_node import LoadTextFromFileNode
 from .single_speaker_node import VibeVoiceSingleSpeakerNode
 from .multi_speaker_node import VibeVoiceMultipleSpeakersNode
+from .free_memory_node import VibeVoiceFreeMemoryNode
 
-__all__ = ['LoadTextFromFileNode', 'VibeVoiceSingleSpeakerNode', 'VibeVoiceMultipleSpeakersNode']
+__all__ = [
+    'LoadTextFromFileNode', 
+    'VibeVoiceSingleSpeakerNode', 
+    'VibeVoiceMultipleSpeakersNode',
+    'VibeVoiceFreeMemoryNode'
+]
